db768488583c098681a37d2c7f5d6ee0b89995e2
Dragan Krstic <info@dragankrstic.com>
2016-08-31 12:00:24 +0200

    Added XML tags, animateEncryption, animateDecryption and fixed some issues

--- app/src/main/java/com/noriax/autotypingtextview/MainActivity.java
+++ app/src/main/java/com/noriax/autotypingtextview/MainActivity.java
@@ -2,14 +2,51 @@ package com.noriax.autotypingtextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    AutoTypeTextView lblTextWithMistakes, lblTextWithoutMistakes, lblTextEncryption, lblTextDecryption;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AutoTypeTextView lblText = (AutoTypeTextView)findViewById(R.id.lblText);
-        lblText.setTextAutoTyping("Ovo je tekst koji ce da se autotypeuje");
+        lblTextWithoutMistakes = (AutoTypeTextView)findViewById(R.id.lblTextWithoutMistakes);
+        lblTextWithMistakes = (AutoTypeTextView)findViewById(R.id.lblTextWithMistakes);
+        lblTextDecryption = (AutoTypeTextView)findViewById(R.id.lblTextDecryption);
+        lblTextEncryption = (AutoTypeTextView)findViewById(R.id.lblTextEncryption);
+
+        lblTextWithoutMistakes.setTypingSpeed(100);
+        lblTextWithMistakes.setTypingSpeed(100);
+        lblTextWithMistakes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lblTextWithMistakes.setTextAutoTyping("Hello World! This is AutoTypeTextView.");
+            }
+        });
+
+        lblTextWithoutMistakes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lblTextWithoutMistakes.setTextAutoTypingWithMistakes("Hello World! This is AutoTypeTextView.", AutoTypeTextView.PRECISSION_MED);
+            }
+        });
+
+        lblTextEncryption.setEncryptionSpeed(30);
+        lblTextEncryption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lblTextEncryption.animateEncryption("Hello World! This is AutoTypeTextView");
+            }
+        });
+
+        lblTextDecryption.setDecryptionSpeed(100);
+        lblTextDecryption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lblTextDecryption.animateDecryption("Hello World! This is AutoTypeTextView");
+            }
+        });
+
     }
 }
